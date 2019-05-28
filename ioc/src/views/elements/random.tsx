d19a6a50a0aab611d244cbe7fa9cb2700a09ca92
Hauke Broer <info@owja.de>
2019-05-28 22:15:34 +0200

    update to @owja/ioc 1.0.0-beta.2

--- ioc/src/views/elements/random.tsx
+++ ioc/src/views/elements/random.tsx
@@ -1,9 +1,10 @@
 import {Component, h} from "preact";
 import {inject, TYPE} from "services/ioc";
+import {NOCACHE} from "@owja/ioc";
 
 export class Random extends Component {
 
-    @inject(TYPE.MyRandomService)
+    @inject(TYPE.MyRandomService, NOCACHE)
     private value!: number;
 
     public render() {
